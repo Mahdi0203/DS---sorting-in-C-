84172231f2bbab8098465e255896fe65f1c47efc
Mah.D <125675444+Mahdi0203@users.noreply.github.com>
2024-03-06 03:09:17 +0600

    selection sort .c

--- selection sort .c
+++ selection sort .c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
-void swap(int *xp, int *yp) {         //you can use this function
+void swap(int *xp, int *yp) {   you can use this function
 	int temp = *xp;
 	*xp = *yp;
 	*yp = temp;
@@ -10,7 +10,7 @@ void swap(int *xp, int *yp) {         //you can use this function
 void selectionSort(int arr[], int n){
 	int i, j, min_idx;
 	for (i = 0; i < n-1; i++) {         // One by one move boundary of unsorted subarray
-		min_idx = i;                      // Find the minimum element in unsorted array
+		min_idx = i;                    // Find the minimum element in unsorted array
 		for (j = i+1; j < n; j++) {
             if (arr[j] < arr[min_idx]) {
                 min_idx = j;
@@ -18,7 +18,7 @@ void selectionSort(int arr[], int n){
 		}
 		// Swap the found minimum element with the first element
 		if(min_idx != i){
-            /* swap(&arr[min_idx], &arr[i]); */    // you can use this function.
+            /* swap(&arr[min_idx], &arr[i]); */     /// you can use this function.
             int temp = arr[min_idx];
             arr[min_idx] = arr[i];
             arr[i] = temp;
@@ -28,10 +28,10 @@ void selectionSort(int arr[], int n){
 
 int main(){
 
-  int n;
-  scanf("%d", &n);
+    int n;
+    scanf("%d", &n);
 
-  int arr[n];
+	int arr[n];
 	for(int i = 0; i < n; i++){
         scanf("%d", &arr[i]);
 	}
@@ -43,6 +43,5 @@ int main(){
         printf("%d ", arr[i]);
 	}
 	printf("\n");
-  
 	return 0;
 }
